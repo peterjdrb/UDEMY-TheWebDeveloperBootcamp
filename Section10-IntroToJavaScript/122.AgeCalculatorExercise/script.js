2a0a1244c8431953b9d5bb01a4ce89fa998eff3d
Peter Robertson <peterjdrb@outlook.com>
2019-01-09 16:10:18 +0000

    typo

--- Section10-IntroToJavaScript/122.AgeCalculatorExercise/script.js
+++ Section10-IntroToJavaScript/122.AgeCalculatorExercise/script.js
@@ -1,4 +1,4 @@
 var age = prompt("Enter your age in years");
 var daysAlive = age * 365.25;
 
-alert("You have been alive for (approximately) " + daysAlive + " days.");
+alert("You have been alive for (roughly) " + daysAlive + " days.");
