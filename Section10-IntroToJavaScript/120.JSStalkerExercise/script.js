5b5488d427f99019218275faca303cfe00e53b31
Peter Robertson <peter.robertson@atradius.com>
2019-01-09 15:28:14 +0000

    JS Stalker Exercise
    Section 10 Lesson 120

---
+++ Section10-IntroToJavaScript/120.JSStalkerExercise/script.js
@@ -0,0 +1,6 @@
+var firstName = prompt("Whit is your first name?");
+var lastName = prompt("Whit is your last name?");
+var age = prompt("Whit is your age?");
+
+console.log("Hello " + firstName + " " + lastName + ".");
+console.log("You are " + age + " year(s) old.");
