667ce8969ec871be77220731c0c36b9973c4bd8b
Peter Robertson <peterjdrb@outlook.com>
2019-01-10 13:21:05 +0000

    Fix redundent semi colons

--- Section11-JSBasicControlFlow/134.WhileLoopsProblemSet/script.js
+++ Section11-JSBasicControlFlow/134.WhileLoopsProblemSet/script.js
@@ -3,7 +3,7 @@ var num1 = Number(-10);
 while (num1 <= 19) {
 	console.log(num1);
 	num1++;
-};
+}
 
 console.log("Print All even numbers between 10 and 40");
 var num2 = Number(10);
@@ -12,7 +12,7 @@ while (num2 <= 40) {
 		console.log(num2);
 	}
 	num2++;
-};
+}
 
 console.log("Print All odd numbers between 300 and 333");
 var num3 = Number(300);
@@ -21,7 +21,7 @@ while (num3 <= 333) {
 		console.log(num3);
 	}
 	num3++;
-};
+}
 
 console.log("Print All numbers divisable by 5 AND 3 between 5 and 50");
 var num4 = Number(5);
