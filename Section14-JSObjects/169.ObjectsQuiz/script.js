d69763c8bbd8d4d74d585765d77ba394a0f37efc
Peter Robertson <peterjdrb@outlook.com>
2019-01-11 12:12:55 +0000

    Objects Quiz
    Seciton 14 Lecture 169

---
+++ Section14-JSObjects/169.ObjectsQuiz/script.js
@@ -0,0 +1,11 @@
+var someObject = {
+	friends: [
+		{name: "Malfoy"},
+		{name: "Crabbe"},
+		{name: "Goyle"}
+	],
+	color: "Bably Blue",
+	isEvil: true
+};
+
+console.log(someObject.friends[0].name);
