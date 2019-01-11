ee6ddf2a73a5046eaaa2d8668f239c50ce7d36c3
Peter Robertson <peterjdrb@outlook.com>
2019-01-11 12:47:12 +0000

    Movies Database
    Section 14 Lecture 170

---
+++ Section14-JSObjects/170.MoviesDatabase/script.js
@@ -0,0 +1,30 @@
+var movies = [
+	{
+		title: "In Burges",
+		rating: 5,
+		hasWatched: true
+	},
+	{
+		title: "Frozen",
+		rating: 4.5,
+		hasWatched: false
+	},
+	{
+		title: "Mad Max Fury Road",
+		rating: 5,
+		hasWatched: true
+	},
+	{
+		title: "Les Miserables",
+		rating: 3.5,
+		hasWatched: false
+	}
+];
+
+movies.forEach(function(movie) {
+	if (movie.hasWatched) {
+		console.log("You have watched \"" + movie.title + "\" - " + movie.rating + " stars");
+	} else {
+		console.log("You have not seen \"" + movie.title + "\" - " + movie.rating + " stars");
+	}
+})
