b8f37a381dbdc2ccec24b36ef0ac1f12dc034dfc
Peter Robertson <peterjdrb@outlook.com>
2019-01-11 15:54:28 +0000

    Selector Exercise
    Section 15 Lecture 177

---
+++ Section15-DOM_Manipulation/177.SelectorExercise/script.js
@@ -0,0 +1,14 @@
+var p1 = document.getElementById("first");
+var p2 = document.querySelector("#first");
+var p3 = document.getElementsByClassName("special")[0];
+var p4 = document.querySelector(".special");
+var p5 = document.getElementsByTagName("p")[0];
+var p6 = document.querySelector("p");
+
+console.log(p1);
+console.log(p1.textContent);
+console.log(p2);
+console.log(p3);
+console.log(p4);
+console.log(p5);
+console.log(p6);
