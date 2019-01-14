385e95ae9909f7d750af6bca8fc33648fb8f4233
Peter Robertson <peterjdrb@outlook.com>
2019-01-14 16:57:35 +0000

    Funcitonality complete.
    Section 17 Lecture 200

--- Section17-ColourGameProject/ColourGame.js
+++ Section17-ColourGameProject/ColourGame.js
@@ -2,14 +2,20 @@ var colours = [];
 var pickedColour;
 var squares = document.querySelectorAll(".square");
 var colourDisplay = document.getElementById("colourDisplay");
-var resultDisplay = document.querySelector("#result")
-var resetButton = document.getElementById("reset");
+var resultDisplay = document.querySelector("#result");
+var btnReset = document.getElementById("reset");
+var btnEasy = document.getElementById("btnEasy");
+var btnHard = document.getElementById("btnHard");
+var totalSquares = 6;
+btnHard.classList.toggle("selected");
+resetGame(totalSquares);
 
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
 function setColours () {
+	squares = document.querySelectorAll(".square");
 	for (i=0; i<squares.length; i++){
 		squares[i].style.background = colours[i];
 
@@ -19,7 +25,7 @@ function setColours () {
 			if (clickedColour === pickedColour) {;
 				resultDisplay.textContent = "Correct";
 				changeColours();
-				resetButton.textContent = "Play Again?";
+				btnReset.textContent = "Play Again?";
 			} else {
 				this.style.background = "#232323";
 				resultDisplay.textContent = "Try Again";
@@ -48,21 +54,54 @@ function generateRandomColours (numOfColours) {
 	var arr = []
 
 	for (i=0; i<numOfColours; i++){
-		arr[i] = "rgb(" + getRndInteger(0,255) + ", " + getRndInteger(0,255) + ", " +getRndInteger(0,255) + ")"
+		arr[i] = "rgb(" + getRndInteger(0,255) + ", " + getRndInteger(0,255) + ", " +getRndInteger(0,255) + ")";
 	}
 
 	return arr;
 }
 
-function resetGame () {
-	resetButton.textContent = "New Game";
-	colours = generateRandomColours(6);
+function resetGame (squareNum) {
+	btnReset.textContent = "New Game";
+	resultDisplay.textContent = "";
+	colours = generateRandomColours(squareNum);
+	setupGameBoard(squareNum);
 	setWinningSquare();
 	setColours();
 }
 
-resetButton.addEventListener("click", function () {
-	resetGame();
+function setupGameBoard (squareNum) {
+	var squareContainer = document.getElementById("squareContainer");
+	squareContainer.innerHTML = "";
+
+	for (i=0; i<totalSquares; i++) {
+		squareContainer.innerHTML += '<div class="square"></div>';
+	}
+}
+
+btnReset.addEventListener("click", function () {
+	if (btnEasy.classList.contains("selected")) {
+		resetGame(totalSquares);
+	} else {
+		resetGame(totalSquares);
+	}
 });
 
-resetGame();
+function difficultySetting (squareNum) {
+	btnEasy.classList.toggle("selected");
+	btnHard.classList.toggle("selected");
+	resetGame(squareNum);
+}
+
+btnEasy.addEventListener("click", function(){
+	if (!btnEasy.classList.contains("selected")) {
+		totalSquares = 3;
+		difficultySetting(totalSquares);
+	};
+})
+
+btnHard.addEventListener("click", function(){
+	if (!btnHard.classList.contains("selected")) {
+		totalSquares = 6;
+		difficultySetting(totalSquares);
+	}
+})
