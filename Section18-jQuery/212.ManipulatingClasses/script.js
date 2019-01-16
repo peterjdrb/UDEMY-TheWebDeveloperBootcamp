b8584c6ec7b04254d8a0c42b44aeee8c1a039393
Peter Robertson <peterjdrb@outlook.com>
2019-01-16 15:46:33 +0000

    Manipulating Classes in jQuery
    Section 18 Lecture 212

---
+++ Section18-jQuery/212.ManipulatingClasses/script.js
@@ -0,0 +1,20 @@
+try	{
+
+	setTimeout(function () {
+		$("img").attr("src", "https://images.unsplash.com/photo-1547479117-60c805aba44d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80")
+		
+	}, 2000)
+
+	setInterval(function() {
+		$("li:first").toggleClass("JS");
+	}, 1000)
+	
+	$("input").val("JSCode done this");
+	var userInput = $("input").val();
+	console.log(userInput);
+
+	
+}
+catch(err){
+	console.log(err.message);
+}
