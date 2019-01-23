4ea5e2f3321dfe0cd0d0a4d79343d431262d9ae3
Peter Robertson <peterjdrb@outlook.com>
2019-01-23 15:49:22 +0000

    Code Refactor

--- Section25-ServerSideFramework/267.ExpressBasicExercise/app.js
+++ Section25-ServerSideFramework/267.ExpressBasicExercise/app.js
@@ -2,19 +2,22 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(req, res) {
-   res.send("Hi there, welcome to my assignment!");
+    res.send("Hi there, welcome to my assignment!");
 });
 
 app.get("/speak/:animal", function(req, res) {
-   var animal = req.params.animal;
-   if (animal === "pig"){
-       res.send("The pig says 'Oink'");
-   } else if (animal === "cow"){
-       res.send("The cow says 'Moo'");
-   } else if (animal === "dog"){
-       res.send("The dog says 'Woof Woof'");
-   }
-   res.send("I don't know what sound a " + animal + " makes");
+    var animal = req.params.animal.toLowerCase();
+    var sounds = {
+        pig:"Oink",
+        cow: "Moo",
+        dog:"Woof Woof!"
+    };
+    
+    var sound = sounds[animal];
+    if (sound === undefined) {
+        res.send("I don't know what sound a  " + animal + " makes!");
+    }
+    res.send("The " + animal + " says '" + sound + "'");
 });
 
 app.get("/repeat/:word/:number", function(req, res){
