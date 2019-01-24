7e14aa0a2254d2488f0459250195c4363e2efc38
Peter Robertson <peterjdrb@outlook.com>
2019-01-24 10:47:21 +0000

    EJS Loops and Conditionals
    Section 26 Lecture 271

---
+++ Section26-IntermediateExpress/271.EJSLoopsAndConditionals/app.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function (req, res){
+    res.render("home.ejs");
+});
+
+app.get("/fallInLove/:thing", function(req, res) {
+    var thing = req.params.thing; 
+    res.render("love.ejs", {thingVar:thing});
+});
+
+app.get("/posts", function(req, res) {
+   var posts = [
+                {title:"Post 1", author:"Albert"},
+                {title:"Post 2", author:"Bert"},
+                {title:"Post 3", author:"Charles"}
+       ];
+    res.render("posts.ejs", {postsEJS:posts});
+});
+
+app.get("*", function (req, res){
+    res.send("Page not found :(");
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log('https://udemy-peterjdrb.c9users.io/');
+});
