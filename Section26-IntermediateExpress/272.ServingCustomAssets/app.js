d84e29ae59caa86205f824f0e95d7f9a420d8c93
Peter Robertson <peterjdrb@outlook.com>
2019-01-24 11:27:00 +0000

    EJS Serving Custom Assets
    Section 26 Lecture 272

---
+++ Section26-IntermediateExpress/272.ServingCustomAssets/app.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res){
+    res.render("home");
+});
+
+app.get("/fallInLove/:thing", function(req, res) {
+    var thing = req.params.thing; 
+    res.render("love", {thingVar:thing});
+});
+
+app.get("/posts", function(req, res) {
+   var posts = [
+                {title:"Post 1", author:"Albert"},
+                {title:"Post 2", author:"Bert"},
+                {title:"Post 3", author:"Charles"}
+       ];
+    res.render("posts", {postsEJS:posts});
+});
+
+app.get("*", function (req, res){
+    res.send("Page not found :(");
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log('https://udemy-peterjdrb.c9users.io/');
+});
